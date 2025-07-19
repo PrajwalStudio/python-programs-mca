f1a7051be3805d7131fdffbf7c1720fd403ad7f2
PrajwalStudio <prajwalpoojary1712@gmail.com>
2025-07-20 01:07:41 +0530

    Array

---
+++ partb/Array.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+r = int(input("Enter number of rows (m): "))
+c = int(input("Enter number of columns (n): "))
+
+array=np.zeros((r,c),dtype=int)
+for i in range(r):
+    for j in range(c):
+        array[i][j]=int(input(f"Enter the element for Row{i} and Column{j}:"))
+
+print("\n Array:")
+print(array)
+
+print("\nArray Attributes:")
+print("Shape          :", array.shape)
+print("Size           :", array.size)
+print("Data type      :", array.dtype)
+print("Dimensions     :", array.ndim)
+print("Item size (in bytes):", array.itemsize)
